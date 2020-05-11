f9cd70aeda01a3b2b4c5ebe7d8bde3231678d9f9
VolodymyrMazurets <volodymyrmazurets@gmail.com>
2020-05-11 14:04:00 +0300

    :sparkles: topic filter

---
+++ src/scenes/home/components/HomeFilterSearch.vue
@@ -0,0 +1,43 @@
+<template>
+  <m-row class="home-filter-search" ai="center">
+    <input placeholder="Search" class="home-filter-search__input" type="text" v-model="search" />
+    <m-icon class="home-filter-search__icon" icon="search" />
+  </m-row>
+</template>
+<script>
+export default {
+  name: 'HomeFilterSearch',
+  props: {
+    value: [String, Array, Number, Boolean],
+  },
+  computed: {
+    search: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
+<style lang="scss">
+$style: home-filter-search;
+.#{$style} {
+  padding: 0 16px;
+  &__input {
+    @extend %input;
+    padding: 3px 0 0;
+    min-height: auto;
+    background-color: transparent;
+    border: none;
+    &:focus + .#{$style}__icon {
+      fill: $G2;
+    }
+  }
+  &__icon {
+    @include svg(16px, $G6);
+  }
+}
+</style>
