cf5f634ee2a6b604ffa6eebef79cf136595ed833
Alex Ratushnyi <alexander.ratushnyi@gmail.com>
2020-05-18 17:37:23 +0300

    🐛Table fix

--- src/scenes/home/table/HomeTableItemWrapper.vue
+++ src/scenes/home/table/HomeTableItemWrapper.vue
@@ -1,7 +1,7 @@
 <template>
   <m-container
     class="home-table-item-wrapper"
-    :contentClass="`home-table-item-wrapper__content ${contentClass}`"
+    :contentClass="`home-table-item-wrapper__content _scroll ${contentClass}`"
   >
     <slot />
   </m-container>
@@ -10,22 +10,33 @@
 export default {
   name: 'HomeTableItemWrapper',
   props: {
-    contentClass: String,
+    contentClass: {
+      type: String,
+      default: '',
+    },
   },
 };
 </script>
 <style lang="scss">
 $style: home-table-item-wrapper;
-@function columns($width: 200px) {
+@function columns($width: 160px) {
   @return 16px #{$width} 85px 85px 75px 70px 75px 100px 70px 100px 110px 70px 100px 42px;
 }
 .#{$style} {
   &__content {
     display: grid;
-    grid-template-columns: columns();
+    grid-template-columns: 18px 150px 90px 50px 50px 50px 60px 110px 70px 100px 110px 70px 100px 40px;
     align-items: center;
     justify-content: space-between;
     column-gap: 5px;
+    &._scroll {
+      & > *:not(:first-child) {
+        overflow-x: auto;
+      }
+    }
+    @include media(1300px) {
+      grid-template-columns: columns();
+    }
     @include media($screen-fullhd) {
       grid-template-columns: columns(210px);
     }
